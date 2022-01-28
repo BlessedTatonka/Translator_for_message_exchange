f9525b954a73d79221df32ccfc8d4c3f5f22cca0
BlessedTatonka <quelquemath@gmail.com>
2022-01-28 03:42:37 +0300

    delete extra files

--- src/test.py
+++
@@ -1,8 +0,0 @@
-# import nemo
-# import nemo.collections.nlp as nemo_nlp
-
-def foo():
-#     nmt_model = nemo_nlp.models.machine_translation.MTEncDecModel.from_pretrained(model_name="nmt_en_ru_transformer6x6")
-#     print(nmt_model)
-    print(1)
-    return 1
